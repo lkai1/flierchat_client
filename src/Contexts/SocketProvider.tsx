e875168aa12a0b2651c445a5a6dd9aa210bc8629
lkai1 <kai-le@outlook.com>
2025-07-29 18:38:18 +0300

    url fix

--- src/Contexts/SocketProvider.tsx
+++ src/Contexts/SocketProvider.tsx
@@ -10,7 +10,7 @@ import { SocketContext } from "./SocketContext.ts";
 //it is most likely now wrong
 const socket = io(
     //production
-    "wss://flierchatserver-production.up.railway.app",
+    "https://flierchatserver-production.up.railway.app",
     //development
     /* "http://localhost:5000", */
     { autoConnect: false, withCredentials: true });
